fcc1656e664cb240ef922e84f380b07f70b3862d
Adiissep <adisepriyadi0@gmail.com>
2026-02-25 15:36:38 +0700

    feat: Add auth middleware to protect master-items and categories routes also fix compatible version of boostrap

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,27 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Routes untuk Master Items
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/master-items', [App\Http\Controllers\MasterItemsController::class, 'index']);
-Route::get('/master-items/search', [App\Http\Controllers\MasterItemsController::class, 'search']);
-Route::get('/master-items/form/{method}/{id?}', [App\Http\Controllers\MasterItemsController::class, 'formView']);
-Route::post('/master-items/form/{method}/{id?}', [App\Http\Controllers\MasterItemsController::class, 'formSubmit']);
-Route::get('/master-items/view/{kode}', [App\Http\Controllers\MasterItemsController::class, 'singleView']);
-Route::get('/master-items/delete/{id}', [App\Http\Controllers\MasterItemsController::class, 'delete']);
-Route::get('/master-items/update-random-data', [App\Http\Controllers\MasterItemsController::class, 'updateRandomData']);
-Route::get('/master-items/export-excel', [App\Http\Controllers\MasterItemsController::class, 'exportExcel']);
 
-// Routes untuk Categories
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/search', [CategoryController::class, 'search']);
-Route::get('/categories/form/{method}/{id?}', [CategoryController::class, 'formView']);
-Route::post('/categories/form/{method}/{id?}', [CategoryController::class, 'formSubmit']);
-Route::get('/categories/view/{kode}', [CategoryController::class, 'singleView']);
-Route::get('/categories/delete/{id}', [CategoryController::class, 'delete']);
-Route::get('/categories/export-pdf/{kode}', [CategoryController::class, 'exportPdf']);
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+    // Routes untuk Master Items
+    Route::get('/master-items', [App\Http\Controllers\MasterItemsController::class, 'index']);
+    Route::get('/master-items/search', [App\Http\Controllers\MasterItemsController::class, 'search']);
+    Route::get('/master-items/form/{method}/{id?}', [App\Http\Controllers\MasterItemsController::class, 'formView']);
+    Route::post('/master-items/form/{method}/{id?}', [App\Http\Controllers\MasterItemsController::class, 'formSubmit']);
+    Route::get('/master-items/view/{kode}', [App\Http\Controllers\MasterItemsController::class, 'singleView']);
+    Route::get('/master-items/delete/{id}', [App\Http\Controllers\MasterItemsController::class, 'delete']);
+    Route::get('/master-items/update-random-data', [App\Http\Controllers\MasterItemsController::class, 'updateRandomData']);
+    Route::get('/master-items/export-excel', [App\Http\Controllers\MasterItemsController::class, 'exportExcel']);
+
+    // Routes untuk Categories
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/search', [CategoryController::class, 'search']);
+    Route::get('/categories/form/{method}/{id?}', [CategoryController::class, 'formView']);
+    Route::post('/categories/form/{method}/{id?}', [CategoryController::class, 'formSubmit']);
+    Route::get('/categories/view/{kode}', [CategoryController::class, 'singleView']);
+    Route::get('/categories/delete/{id}', [CategoryController::class, 'delete']);
+    Route::get('/categories/export-pdf/{kode}', [CategoryController::class, 'exportPdf']);
+});
